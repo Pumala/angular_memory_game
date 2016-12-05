b96878253d719bd7ff3830549276453ed8c6cce3
Carolyn Lam <caro_lyn_@hotmail.com>
2016-12-05 13:02:21 -0500

    add more comments to js file

--- memorygame.js
+++ memorygame.js
@@ -22,27 +22,24 @@ app.controller('MemoryController', function($scope, $timeout) {
     if ((card.matched === false) && ($scope.gameInPlay === true)) {
       // if this is the first state, then this is the first card we need to compare
       if ($scope.firstState === true) {
-        // we keep the first picked card open
-        card.open = true;
         // we set the card as the first picked card
         $scope.firstCard = card;
+        // we keep the first picked card open
+        $scope.firstCard.open = true;
         // we reset first card value to null
         $scope.firstState = null;
         // if we are not in the first state we check this condition
         // it checks if the first card picked is not the same exact card
         // as the current card picked (checks to make sure we don't pick the same card)
       } else if ($scope.firstCard['$$hashKey'] !== card['$$hashKey']) {
-        // we keep the 2nd card picked open
-        card.open = true;
         // we set the 2nd card picked as the second picked card
         $scope.secondCard = card;
+        // we keep the 2nd card picked open
+        $scope.secondCard.open = true;
         // we update the moves count by 1
         $scope.deck.moves += 1;
         // check if the first card image is the same as the second card image
         if ($scope.firstCard.url === $scope.secondCard.url) {
-          // keep the first card and second card open
-          $scope.firstCard.open = true;
-          $scope.secondCard.open = true;
           // set the first card and second card matched value to true
           $scope.firstCard.matched = true;
           $scope.secondCard.matched = true;
@@ -56,17 +53,18 @@ app.controller('MemoryController', function($scope, $timeout) {
           }
           // if the cards are not a match
         } else {
-          // this part below uses delays 500 seconds
-          // then sets gameInPlay to true
-          // hides the first card and second card
-          // this part is important because it disables the ability to click cards
-          //
+          // this part below sets a delay of 500 seconds
+          // then hides the first card and second card
           $timeout(function() {
-            $scope.gameInPlay = true;
             $scope.firstCard.open = false;
             $scope.secondCard.open = false;
           }, 500);
         }
+        // now that the second card has been picked
+        // we set firstState to true
+        // sets a delay of 500 seconds, then sets gameInPlay from false to true
+        // this is important because it disables the ability to click other cards while game is in progress
+        // reset value of first card and second card to null
         $scope.firstState = true;
         $scope.gameInPlay = false;
         $timeout(function() {
@@ -79,12 +77,14 @@ app.controller('MemoryController', function($scope, $timeout) {
   }
 });
 
+// create a Card Constructor
 function Card(url, open, matched) {
   this.url = url;
   this.open = open;
   this.matched = matched;
 }
 
+// create a Deck Constructor
 function Deck() {
   this.currentCards = [];
   this.generateCards();
@@ -93,6 +93,8 @@ function Deck() {
   this.won = false;
 }
 
+// create a Deck method that generates cards
+// you pass numCards (the amount of cards in the deck)
 Deck.prototype.generateCards = function(numCards) {
   var cards = [];
   this.currentCards = [];
